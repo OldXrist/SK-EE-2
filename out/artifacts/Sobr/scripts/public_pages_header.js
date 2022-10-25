d867d3b98889bc1f8faf83d189cc17e43e82f98d
Igor <igor.spiridonovv@bk.ru>
2022-10-25 10:43:21 +0300

    красота

---
+++ out/artifacts/Sobr/scripts/public_pages_header.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+    $(".nav_login").hide()
+}).ready(function () {
+    $("#clock").hide()
+}).ready(function () {
+    $("#logout_btn").hide()
+}).ready(function (){
+    $(".logout").hide()
+});
+
+$.get("http://localhost:8080/Sobr/MAINServlet", function (data) {
+    console.log(data)
+    console.log(typeof data)
+
+    if (data !== "") {
+        $(".nouser").hide()
+        $(".nav_login").show()
+        $("#clock").show()
+        $("#logout_btn").show()
+        $(".logout").show()
+        document.getElementById("lk").innerHTML = data
+    }
+})
