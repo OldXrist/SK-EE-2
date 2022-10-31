d517db76bcdd30ea32ead40b84c36c624019ff14
mackcik <112466343+mackcik2@users.noreply.github.com>
2022-10-31 15:42:22 +0300

    6565

--- src/main/java/email/email.java
+++ src/main/java/email/email.java
@@ -1,62 +1,65 @@
 package email;
 
-import jakarta.servlet.annotation.*;
-import java.util.Properties;
-import jakarta.mail.Message;
-import jakarta.mail.MessagingException;
-import jakarta.mail.PasswordAuthentication;
-import jakarta.mail.internet.*;
+import java.io.IOException;
+import java.util.*;
 import jakarta.mail.*;
+import jakarta.mail.internet.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "email", value = "/email")
-public class email {
-
-
-
-    public static void main(String[] args) {
-
-        Properties p = new Properties();
-        p.put("mail.smtp.host", "smtp.gmail.com");
-        p.put("mail.smtp.socketFactory.port", 587);
-        p.put("mail.smtp.socketFactory.class", "jakarta.net.ssl.SSLSocketFactory");
-        p.put("mail.smtp.auth", "true");
-        p.put("mail.smtp.port", 587);
-
+/**
+ * Servlet implementation class email
+ */
+@WebServlet("/email")
+public class email extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
 
-
-        Session s = Session.getInstance(p,
-
-                new jakarta.mail.Authenticator(){
-
-
-                    protected PasswordAuthentication getPasswordAuthentication(){
-
-                        return new PasswordAuthentication("mackcik228225@gmail.com", "chndipgjwleldyfj");
-
-                    }
+    public email() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 
-                }
-        );
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+
+    }
 
 
-        Message message = new MimeMessage(s);
-        try {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        try
+        {
+            Properties props = new Properties();
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.class", "jakarta.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "465");
+
+            Session session = Session.getDefaultInstance(props,
+                    new jakarta.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication("mackcik228225@gmail.com","ywvhajgacbnwxxmh");
+                        }
+                    });
+
+            Message message=new MimeMessage(session);
             message.setFrom(new InternetAddress("mackcik228225@gmail.com"));
-            message.setRecipient(Message.RecipientType.TO, new InternetAddress("max260902@mail.ru"));
-            message.setSubject("Тема письма");
-            message.setText("Сообщение в письме");
-
+            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse("max260902@mail.ru"));
+            message.setSubject("Agro Test");
+            message.setContent("xyi\n"
+                    , "text/html");
             Transport.send(message);
-
-            System.out.println("Письмо успешно отправлено");
-
-        } catch (MessagingException e) {
-            System.out.println("Письмо не отправилось");
-            e.printStackTrace();
+            System.out.println("message sent....");
+        } catch(Exception ex)
+        {
+            ex.printStackTrace();
         }
-
     }
 
 }
