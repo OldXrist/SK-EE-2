4331f241be8dd31da09b6c7bd7e8b580f4e6c38e
Igor <igor.spiridonovv@bk.ru>
2022-11-17 16:25:04 +0300

    pagination(final)

--- src/main/webapp/scripts/get_reestr_lk_org.js
+++ src/main/webapp/scripts/get_reestr_lk_org.js
@@ -1,23 +1,147 @@
-function page(id){
-    let k = 75 * (parseInt(id.split('_')[1]) - 1)
-    document.getElementById('table').innerHTML = ''
+function goTO(page){
+    let next = 0
     let i = 1
+    while (document.getElementById('c'+i) !== null){
+        document.getElementById('c'+i).style.display = 'none'
+        i++
+    }
+    for (let i = 1; i < 6; i++){
+        let first = 1+5*(page - 1)
+        console.log('first = '+first)
+        if (next < first + 4) {
+            next = first + i
+        }
+        console.log('next = '+next)
+        //let prev = first - i
+        //console.log('prev = '+prev)
+        if (document.getElementById('c' + next) !== null) {
+            document.getElementById('c' + next).style.display = ''
+        }
+        //if (document.getElementById('c' + prev) !== null) {
+        //    document.getElementById('c' + prev).style.display = 'none'
+        //}
+        document.getElementById('c' + first).style.display = ''
+    }
+}
+
+function page(id){
+    //let k = 75 * (parseInt(id.split('_')[1]) - 1)
+    //document.getElementById('table').innerHTML = ''
+    let i = 1
+
     while (document.getElementById('page_'+i) !== null){
         document.getElementById('page_'+i).style.color = '#104781'
-        document.getElementById('page_'+i).style.backgroundColor = '#ffffff'
+        document.getElementById('page_'+i).style.background = '#ffffff'
         i++
     }
 
-    document.getElementById(id).style.color = '#ffffff'
-    document.getElementById(id).style.backgroundColor = '#104781'
+    if (id.split('_')[0] === 'page') {
+        document.getElementById(id).style.color = '#ffffff'
+        document.getElementById(id).style.background = '#104781'
+    }
 
+    if (id.split('_')[0] === 'next') {
+        let x = parseInt(id.split('_')[1])
+        let n = 1
+        document.getElementById('next_'+x).style.display = 'none'
+        while(document.getElementById('page_'+n) !== null){
+            document.getElementById('page_'+n).style.display = 'none'
+            n++
+        }
+        for (let i = 1; i < 3; i++){
+            let next = x+i
+            if (document.getElementById("page_" + next) !== null) {
+                document.getElementById("page_" + next).style.display = ''
+            }
+        }
+        let newNext = x + 3
+        let lastPrev = x - 3
+        if (document.getElementById("prev_" + lastPrev) !== null) {
+            document.getElementById("prev_" + lastPrev).style.display = 'none'
+        }
+        document.getElementById('prev_'+x).style.display = ''
+        document.getElementById('next_'+newNext).style.display = ''
+        document.getElementById('page_'+x).style.display = ''
+        /*
+        let x = parseInt(id.split('_')[1])
+        document.getElementById(id).style.display = 'none'
+        document.getElementById('prev_'+x).style.display = ''
+        document.getElementById('page_'+x).style.display = ''
+        for (let i = 1; i < 4; i++){
+            let prev = x-i
+            let next = x+i
+            document.getElementById("page_" + prev).style.display = 'none'
+            if (document.getElementById("page_" + next) !== null) {
+                document.getElementById("page_" + next).style.display = ''
+            }
+        }
+         */
+    } else if (id.split('_')[0] === 'prev'){
+        let x = parseInt(id.split('_')[1])
+        let n = 1
+        document.getElementById('prev_'+x).style.display = 'none'
+        while(document.getElementById('page_'+n) !== null){
+            document.getElementById('page_'+n).style.display = 'none'
+            n++
+        }
+        for (let i = 1; i < 4; i++){
+            let prev = x-i
+            if (document.getElementById("page_" + prev) !== null) {
+                document.getElementById("page_" + prev).style.display = ''
+            }
+        }
+        let lastNext = x + 3
+        let newPrev = x - 3
+        if (document.getElementById("next_" + lastNext) !== null) {
+            document.getElementById("next_" + lastNext).style.display = 'none'
+        }
+        document.getElementById('next_'+x).style.display = ''
+        if (document.getElementById('prev_'+newPrev) !== null) {
+            document.getElementById('prev_' + newPrev).style.display = ''
+        }
+        document.getElementById('page_'+x).style.display = 'none'
+        /*
+        let x = parseInt(id.split('_')[1])
+        document.getElementById(id).style.display = 'none'
+        document.getElementById('next_'+x).style.display = ''
+        document.getElementById('page_'+x).style.display = 'none'
+        for (let i = 1; i < 4; i++){
+            let prev = x-i
+            let next = x+i
+            document.getElementById("page_" + prev).style.display = ''
+            if (document.getElementById("page_" + next) !== null) {
+                document.getElementById("page_" + next).style.display = 'none'
+            }
+        }
+
+         */
+    }
+/*
     $.get("http://localhost:8080/Sobr/LKUCHServlet", function (data){
         let d = data.split("\n")
-        for (let i = 1; i < 6; i++) {
-            Cards(d, k, i)
-            k += 15
+        if (document.getElementById('table').innerHTML === '') {
+            for (let i = 1; i < 6; i++) {
+                Cards(d, k, i)
+                k += 15
+            }
         }
     })
+ */
+    let pageNum = parseInt(id.split('_')[1])
+    let prefix = id.split('_')[0]
+
+    if (prefix === 'page'){
+        goTO(pageNum)
+    } else if (prefix === 'next'){
+        goTO(pageNum)
+        document.getElementById('page_' + pageNum).style.color = '#ffffff'
+        document.getElementById('page_' + pageNum).style.background = '#104781'
+    } else {
+        goTO(pageNum - 1)
+        let n = pageNum - 1
+        document.getElementById('page_' + n).style.color = '#ffffff'
+        document.getElementById('page_' + n).style.background = '#104781'
+    }
 }
 
 function dateParse (x){
@@ -37,6 +161,7 @@ function timeParse (x){
 }
 
 function Cards(dt, i, m) {
+    m = "c" + m
     let lim = i + 15;
     let arr = [];
     for (i; i < lim; i++) {
@@ -127,8 +252,8 @@ function Cards(dt, i, m) {
                 "                        <li class=\"thin_text\">Даты приема заявок</li>\n" +
                 "                        <li>" + date2 + "</li>\n" +
                 "                    </ul>\n" +
-                "</div>\n" +
-                "            </div>"
+                "                    </div>\n" +
+                "               </div>"
 
         } else {
             document.getElementById("table").innerHTML += "<div id = " + m + " class=\"table_item\" onclick='Redirect(this.id)'>\n" +
@@ -164,22 +289,67 @@ $.get("http://localhost:8080/Sobr/LKUCHServlet", function (data){
 
     console.log(data)
     let d = data.split("\n")
-    let pageNum = 3//Math.ceil(d.length / 15 / 5)
+    let pageNum = 20//Math.ceil(d.length / 15 / 5)
     console.log(pageNum)
     let k = 0
-    for (let i = 1; i < 6; i++) {
+    for (let i = 1; i < Math.ceil(d.length / 15); i++) {
         Cards(d, k, i)
         k += 15
     }
 
+    let pageLim = 4
+
+    for (let i  =  6; i < Math.ceil(d.length / 15); i++){
+        document.getElementById('c'+i).style.display = 'none'
+    }
+
+
     switch (true){
         case pageNum === 1:
             break;
-        case pageNum < 4:
-            for (let i = 1; i < pageNum + 1; i++){
+        case pageNum < pageLim:
+            for (let i = 1; i < pageLim; i++){
                 document.getElementById('pages').innerHTML += `<li class="pages" id=\"page_${i}\" onclick=\"page(this.id)\">${i}</li>`
             }
             break;
+        case pageNum >= pageLim:
+            let x = 1
+            for (let i = 1; i < pageNum + 1; i++){
+                if (i === x + 3){
+                    document.getElementById('pages').innerHTML += `<li class="pages" id=\"prev_${i}\" onclick=\"page(this.id)\">...</li>`
+                    document.getElementById('pages').innerHTML += `<li class="pages" id=\"next_${i}\" onclick=\"page(this.id)\">...</li>`
+                    document.getElementById('pages').innerHTML += `<li class="pages" id=\"page_${i}\" onclick=\"page(this.id)\">${i}</li>`
+                    x += 3
+                    console.log(x)
+                } else document.getElementById('pages').innerHTML += `<li class="pages" id=\"page_${i}\" onclick=\"page(this.id)\">${i}</li>`
+            }
+            x = 1
+            for (let i = 4; i < pageNum + 1; i++) {
+                if (i === x + 3) {
+                    document.getElementById('prev_'+i).style.display = 'none'
+                    document.getElementById('next_'+i).style.display = 'none'
+                    document.getElementById("page_" + i).style.display = 'none'
+                    x += 3
+                } else document.getElementById("page_" + i).style.display = 'none'
+            }
+            document.getElementById('next_4').style.display = ''
+            break;
+            /*
+            let i
+            for (i = 1; i < pageLim; i++){
+                document.getElementById('pages').innerHTML += `<li class="pages" id=\"page_${i}\" onclick=\"page(this.id)\">${i}</li>`
+            }
+            console.log(i)
+            document.getElementById('pages').innerHTML += `<li class="pages" id=\"page_${i+1}\" onclick=\"page(this.id)\">...</li>`
+            $(`#page_${i+1}`).click(function (){
+                document.getElementById('pages').innerHTML = ''
+                for (i; i < pageNum + 1; i++){
+                    document.getElementById('pages').innerHTML += `<li class="pages" id=\"page_${i}\" onclick=\"page(this.id)\">${i}</li>`
+                }
+                document.getElementById('pages').innerHTML += `<li class="pages" id=\"page_${i+1}\" onclick=\"page(this.id)\">...</li>`
+                console.log(i)
+            })
+             */
     }
 
     document.getElementById('page_1').style.color = '#ffffff'
