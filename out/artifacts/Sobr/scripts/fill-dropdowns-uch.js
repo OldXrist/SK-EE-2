cea6988d0bcbf2242af107adbe1ce564cb989ff2
Иванов Александр <alexdevp90@gmail.com>
2022-12-11 18:10:07 +0300

    Dropdowns fixed

---
+++ out/artifacts/Sobr/scripts/fill-dropdowns-uch.js
@@ -0,0 +1,146 @@
+$(document).ready(function () {
+    $(".tbl").hide();
+
+    $(".show").click(function () {
+        $(".tbl").slideDown()
+    }).click(function () {
+        $(".show").hide()
+    }).click(function () {
+        $(".base_bg").animate({
+            marginTop: "1287px"
+        });
+    }).click(function () {
+        $(".base").animate({
+            marginTop: "1287px"
+        });
+    });
+
+    $(".hide").click(function () {
+        $(".tbl").slideUp()
+    }).click(function () {
+        $(".show").fadeIn()
+    }).click(function () {
+        $(".base_bg").animate({
+            marginTop: "960px"
+        });
+    });
+
+    let url = location.href;
+    let meetingNumber = url.slice(url.lastIndexOf('=') + 1, url.length)
+    let inputData = {number: meetingNumber}
+
+    $.post("http://localhost:8080/Sobr/GetQuestionsServlet", inputData, function (result) {
+        console.log(result);
+
+        //сначала заполняет options
+        var select = document.getElementById('sources-1');
+        for (var i = 0; i < result.length; i++) {
+            select.innerHTML += '<option value="' + result[i] + '">' + result[i] + '</option>';
+        }
+
+        //далее рендерим кастомный select и заполняем данными из options
+        var click = 0;
+        $(".custom-select-1").each(function () {
+            console.log('selection');
+            var classes = $(this).attr("class"),
+                id = $(this).attr("id"),
+                name = $(this).attr("name");
+            var template = '<div class="' + classes + '">';
+            template += '<span class="custom-select-trigger-1">' + $(this).attr("placeholder") + '</span>';
+            template += '<div class="custom-options-1">';
+            $("#sources-1").find("option").each(function () {
+                template += '<span class="custom-option-1 ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+            });
+            template += '</div></div>';
+
+            $(this).wrap('<div class="custom-select-wrapper-1"></div>');
+            $(this).hide();
+            $(this).after(template);
+        });
+        $(".custom-option-1:first-of-type").hover(function () {
+            $(this).parents(".custom-options-1").addClass("option-hover-1");
+        }, function () {
+            $(this).parents(".custom-options-1").removeClass("option-hover-1");
+        });
+        $(".custom-select-trigger-1").on("click", function () {
+            if (click == 1) {
+                $("#members-dropdown").show();
+                click--;
+            } else if (click == 0) {
+                $("#members-dropdown").hide();
+                click++;
+            }
+            $('html').one('click', function () {
+                $(".custom-select-1").removeClass("opened-1");
+            });
+            $(this).parents(".custom-select-1").toggleClass("opened-1");
+
+            event.stopPropagation();
+        });
+        $(".custom-option-1").on("click", function () {
+            $("#members-dropdown").show();
+            click = 0;
+            $(this).parents(".custom-select-wrapper-1").find("select").val($(this).data("value"));
+            $(this).parents(".custom-options-1").find(".custom-option-1").removeClass("selection");
+            $(this).addClass("selection");
+            $(this).parents(".custom-select-1").removeClass("opened-1");
+            $(this).parents(".custom-select-1").find(".custom-select-trigger-1").text($(this).text());
+        });
+    });
+
+
+    $.post("http://localhost:8080/Sobr/GetMembersServlet", inputData, function (result) {
+        console.log(result);
+
+        //сначала заполняет options
+        var select = document.getElementById('sources-2');
+        for (var i = 0; i < result.length; i++) {
+            select.innerHTML += '<option value="' + result[i][0] + ' ' + result[i][1] + ' ' + result[i][2] + '">' + result[i][0] + ' ' + result[i][1] + ' ' + result[i][2] + '</option>';
+        }
+
+        //далее рендерим кастомный select и заполняем данными из options
+        $(".custom-select-2").each(function () {
+            var classes = $(this).attr("class"),
+                id = $(this).attr("id"),
+                name = $(this).attr("name");
+            var template = '<div class="' + classes + '">';
+            template += '<span class="custom-select-trigger-2">' + $(this).attr("placeholder") + '</span>';
+            template += '<div class="custom-options-2">';
+            $("#sources-2").find("option").each(function () {
+                template += '<span class="custom-option-2 ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+            });
+            template += '</div></div>';
+
+            $(this).wrap('<div class="custom-select-wrapper-2"></div>');
+            $(this).hide();
+            $(this).after(template);
+        });
+        $(".custom-option-2:first-of-type").hover(function () {
+            $(this).parents(".custom-options-2").addClass("option-hover-2");
+        }, function () {
+            $(this).parents(".custom-options-2").removeClass("option-hover-2");
+        });
+        $(".custom-select-trigger-2").on("click", function () {
+            $('html').one('click', function () {
+                $(".custom-select-2").removeClass("opened-2");
+            });
+            $(this).parents(".custom-select-2").toggleClass("opened-2");
+
+            event.stopPropagation();
+        });
+        $(".custom-option-2").on("click", function () {
+            $(this).parents(".custom-select-wrapper-2").find("select").val($(this).data("value"));
+            $(this).parents(".custom-options-2").find(".custom-option-2").removeClass("selection");
+            $(this).addClass("selection");
+            $(this).parents(".custom-select-2").removeClass("opened-2");
+            $(this).parents(".custom-select-2").find(".custom-select-trigger-2").text($(this).text());
+        });
+    });
+});
+
+//clock
+setInterval(function () {
+    var now = new Date();
+    var clock = document.getElementById("clock");
+    clock.innerHTML = now.toLocaleTimeString();
+}, 1000);
---
+++ src/main/java/id/check/servlets/cards/GetMembersServlet.java
@@ -0,0 +1,130 @@
+package id.check.servlets.cards;
+
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+@WebServlet("/GetMembersServlet")
+public class GetMembersServlet extends HttpServlet {
+    public String FlSql(String meetingNumber)
+    {
+        String flSql =  "SELECT DISTINCT fl.famil, fl.name, fl.otch\n" +
+                        "FROM public.uch\n" +
+                        "INNER JOIN public.fl ON uch.email = fl.email and fl.type_users = 'участник' and uch.id = '" + meetingNumber + "';";
+        return flSql;
+    }
+
+    public String IpSql(String meetingNumber)
+    {
+        String ipSql =  "SELECT DISTINCT ip.famil, ip.name, ip.otch\n" +
+                        "FROM public.uch\n" +
+                        "INNER JOIN public.ip ON uch.email = ip.email and ip.type_users = 'участник' and uch.id = '" + meetingNumber + "';";
+        return ipSql;
+    }
+
+    public String QlSql(String meetingNumber)
+    {
+        String qlSql =  "SELECT DISTINCT ql.poln_naim\n" +
+                        "FROM public.uch\n" +
+                        "INNER JOIN public.ql ON uch.email = ql.email and ql.type_users = 'участник' and uch.id = '" + meetingNumber + "';";
+        return qlSql;
+    }
+
+    public void GetRolesAndOrganizerEmails(Connection _c, HashSet _userRoles, String _meetingNumber) {
+        try {
+            String sql = "SELECT type_uch, email FROM uch WHERE id = " + _meetingNumber + ";";
+            PreparedStatement ps = _c.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            ResultSetMetaData meta = rs.getMetaData();
+            int columnCount = meta.getColumnCount();
+            while (rs.next()) {
+                ArrayList<String> row = new ArrayList<String>();
+                for (int i = 1; i <= columnCount; ++i)
+                {
+                    Object value = rs.getObject(i);
+                    row.add(String.valueOf(value));
+                }
+                _userRoles.add(row);
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public void SetMembersInfo(Connection _c, String _sql, HashSet<ArrayList<String>> _membersList)
+    {
+        try {
+            PreparedStatement ps = _c.prepareStatement(_sql);
+            ResultSet rs = ps.executeQuery();
+            ResultSetMetaData meta = rs.getMetaData();
+            int columnCount = meta.getColumnCount();
+            while (rs.next()) {
+                ArrayList<String> bankruptcyProcedureInfo = new ArrayList<String>();
+                for (int i = 1; i <= columnCount; ++i)
+                {
+                    Object value = rs.getObject(i);
+                    bankruptcyProcedureInfo.add(String.valueOf(value));
+                }
+                _membersList.add(bankruptcyProcedureInfo);
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String meetingNumber = request.getParameter("number");
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/SK", "postgres", "111");
+            //Connection c = DriverManager.getConnection("jdbc:postgresql://192.168.1.115/postgres", "postgres", "postgresql");
+
+            //собираем роли
+            HashSet<ArrayList<String>> userRoles = new HashSet<ArrayList<String>>();
+            GetRolesAndOrganizerEmails(c, userRoles, meetingNumber);
+
+            //собираем имена участников
+            HashSet<ArrayList<String>> membersList = new HashSet<ArrayList<String>>();
+            for (ArrayList<String> item : userRoles) {
+                switch (item.get(0)) {
+                    case "ФЛ": SetMembersInfo(c, FlSql(meetingNumber), membersList);
+                        break;
+                    case "ИП": SetMembersInfo(c, IpSql(meetingNumber), membersList);
+                        break;
+                    case "ЮЛ": SetMembersInfo(c, QlSql(meetingNumber), membersList);
+                        break;
+                }
+            }
+
+            //возвращаем данные на фронтенд в формате json
+            String json = new Gson().toJson(membersList);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(json);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+}
